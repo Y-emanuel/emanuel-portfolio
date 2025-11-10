7724b27ec3aa8566647701966739d6500c60d2a6
Y-emanuel <brianybalo70@gmail.com>
2025-11-10 06:55:08 -0300

    fix: estructura de main y limpieza de imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import "@fontsource/inter";           
-import "@fontsource/inter/600.css";    
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
+import ErrorBoundary from "./components/ErrorBoundary.jsx";
+import "./index.css";
 
-import ErrorBoundary from './components/ErrorBoundary.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ErrorBoundary> {/* ← Envuelve App */}
+    <ErrorBoundary>
       <App />
     </ErrorBoundary>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
