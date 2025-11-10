cd5e02014ca850b39d7abd8032028329c2cff6dd
Y-emanuel <brianybalo70@gmail.com>
2025-11-10 19:16:04 -0300

    fix: deploy config for Vercel + Vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "@fontsource/inter/400.css"
 import "@fontsource/inter/600.css"
 import "@fontsource/inter/700.css"
 
