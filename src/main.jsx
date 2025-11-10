331924556e1d836356b527980bccba54f1d7f51b
Y-emanuel <brianybalo70@gmail.com>
2025-11-10 07:32:42 -0300

    Fix deploy: remove fontsource and use Google Fonts

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "@fontsource/inter/400.css";
-import "@fontsource/inter/600.css";
-import "@fontsource/inter/700.css";
 
 import ErrorBoundary from "./components/ErrorBoundary.jsx";
 import "./index.css";
