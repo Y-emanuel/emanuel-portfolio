2ab001efbd51b314a99523a9dbfd77251bde334b
Y-emanuel <brianybalo70@gmail.com>
2025-11-10 07:28:02 -0300

    prueba

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        "@fontsource/inter",
+        "@fontsource/inter/400.css",
+        "@fontsource/inter/600.css",
+        "@fontsource/inter/700.css",
+      ]
+    }
+  }
 })
