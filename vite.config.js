dfdc2b277f91d6f4b0f354c69e07f14994f2633a
Y-emanuel <brianybalo70@gmail.com>
2025-11-12 20:05:59 -0300

    Fix vercel build config for Vite

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // 👈 importante
   build: {
     rollupOptions: {
       external: [
         "@fontsource/inter",
         "@fontsource/inter/400.css",
         "@fontsource/inter/600.css",
-        "@fontsource/inter/700.css",
+        "@fontsource/inter/700.css"
       ]
     }
   }
